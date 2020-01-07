0cccbdaeca7fc05b2129c2b2d5434d8988477261
john2 <john2.0zero@gmail.com>
2020-01-07 13:42:25 -0500

    Added model, view and controller files to the game, and put in important buttons for game

---
+++ src/main/java/files/sudoku_view.java
@@ -0,0 +1,12 @@
+package files;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.io.*;
+import javax.imageio.*;
+
+
+public class view {
+}
